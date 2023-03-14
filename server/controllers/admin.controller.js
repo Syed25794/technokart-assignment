3c460a7f67de5b82364594299e1e9caf25578269
Sayyad Gul Mohammad <syedgulmohammad25794@gmail.com>
2023-03-14 19:12:55 +0530

    added edit and delete functionality

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -3,41 +3,44 @@ const SuperAdmin = require("../models/superAdmin.models");
 const bcrypt = require("bcrypt");
 
 const loginSuperAdmin = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
   try {
-    const admin = await SuperAdmin.findOne({email});
-    const isValid = await bcrypt.compare(password,admin.password);
-    res.status(200).send({isValid});
+    const admin = await SuperAdmin.findOne({ email });
+    const isValid = await bcrypt.compare(password, admin.password);
+    res.status(200).send({ isValid });
   } catch (error) {
-    res.status(400).send({error:error.message});
+    res.status(400).send({ error: error.message });
   }
 };
 
-const getPartners = async ( req, res ) =>{
-    try{
-        let partners = await Partner.find();
-        res.status(200).send({data:partners});
-    }catch(error){
-        res.status(400).send({error:error.message});
-    }
-}
+const getPartners = async (req, res) => {
+  const { page, limit } = req.query;
+  let limitPartner = limit;
+  let skips = 10 * (Number(page) - 1);
+  try {
+    let partners = await Partner.find().skip(skips).limit(limitPartner);
+    res.status(200).send({ data: partners });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
 
 const createSuperAdmin = async (req, res) => {
-    let { adminName, email, password } = req.body;
-    bcrypt.hash(password,5,async(error,hashedPassword)=>{
-        if( error ){
-            return res.status(400).send({error:error.message});
-        }
-        password = hashedPassword;
-    });
+  let { adminName, email, password } = req.body;
+  bcrypt.hash(password, 5, async (error, hashedPassword) => {
+    if (error) {
+      return res.status(400).send({ error: error.message });
+    }
+    password = hashedPassword;
+  });
   try {
     const admin = await SuperAdmin.findOne();
     if (admin) {
-        const updateAdmin = await SuperAdmin.updateOne({
-            adminName,
-            email,
-            password
-        });
+      const updateAdmin = await SuperAdmin.updateOne({
+        adminName,
+        email,
+        password,
+      });
     } else {
       const superAdmin = new SuperAdmin({
         adminName,
@@ -48,26 +51,54 @@ const createSuperAdmin = async (req, res) => {
     }
     res.status(201).send({ message: "Super Admin created successfully" });
   } catch (error) {
-    res
-      .status(400)
-      .send({ message: "something went wrong in creating super admin." });
+    res.status(400).send({ message: "something went wrong in creating super admin." });
   }
 };
 
-const addPartner = async ( req, res )=>{
-    const { partner_name , partner_email } = req.body;
-    const login_link = `localhost:3000/${partner_name}/login`;
-    try {
-        const newPartner = new Partner({
-            partner_name,
-            partner_email,
-            login_link
-        });
-        newPartner.save();
-        res.status(201).send({"message":"Partner is created successfully."});
-    } catch (error) {
-        res.status(400).send({error:error.message});
-    }
-}
+const addPartner = async (req, res) => {
+  const { partner_name, partner_email } = req.body;
+  const login_link = `localhost:3000/${partner_name}/login`;
+  try {
+    const newPartner = new Partner({
+      partner_name,
+      partner_email,
+      login_link,
+    });
+    newPartner.save();
+    res.status(201).send({ message: "Partner is created successfully." });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
 
-module.exports = { loginSuperAdmin, createSuperAdmin, getPartners, addPartner };
+const editPartnerDetails = async (req, res) => {
+  const { email, newName, newEmail } = req.body;
+  try {
+    const result = await Partner.updateOne(
+      { email },
+      { partner_email: newEmail, partner_name: newName }
+    );
+    res.status(202).send({ result });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
+
+const deletePartner = async (req, res) => {
+  const { email } = req.body;
+  try {
+    let result = await Partner.deleteOne({email});
+    res.status(202).send({result});
+  } catch (error) {
+    res.status(400).send({error:error.message})
+  }
+};
+
+module.exports = {
+  loginSuperAdmin,
+  createSuperAdmin,
+  getPartners,
+  addPartner,
+  editPartnerDetails,
+  deletePartner,
+};
---
+++ server/controllers/partner.controller.js
@@ -0,0 +1,5 @@
+const generateOtp = async ( req, res )=>{
+    const { email } = req.body;
+}
+
+module.exports = { generateOtp };
