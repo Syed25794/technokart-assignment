47e566500a70b9676b3928da2a1154fcfae3ae28
Sayyad Gul Mohammad <syedgulmohammad25794@gmail.com>
2023-03-14 23:08:14 +0530

    added otp generation route

--- server/routes/partner.routes.js
+++ server/routes/partner.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
+const { generateOtp } = require("../controllers/partner.controller");
 
 const partnerRoutes = Router();
 
-partnerRoutes.post("/generateOtp");
+partnerRoutes.post("/generateOtp",generateOtp);
 partnerRoutes.post("/login");
 partnerRoutes.post("/add_event");
 partnerRoutes.get("/thankyoupage");
