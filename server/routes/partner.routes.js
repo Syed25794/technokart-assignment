07b977d9450ae924b8d11c4ea9c47a571bc5ccac
Sayyad Gul Mohammad <syedgulmohammad25794@gmail.com>
2023-03-16 09:50:48 +0530

    some fix

--- server/routes/partner.routes.js
+++ server/routes/partner.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const { generateOtp, loginPartner, addEvent } = require("../controllers/partner.controller");
-const redirectEventPage = require("../middlewares/redirectEventPage");
-const redirectLoginPage = require("../middlewares/redirectLoginPage");
+// const redirectEventPage = require("../middlewares/redirectEventPage");
+// const redirectLoginPage = require("../middlewares/redirectLoginPage");
 
 const partnerRoutes = Router();
 
 partnerRoutes.post("/generateOtp",generateOtp);
-partnerRoutes.post("/login",redirectEventPage,loginPartner);
-partnerRoutes.post("/add_event",redirectLoginPage,addEvent);
+partnerRoutes.post("/login",loginPartner);
+partnerRoutes.post("/add_event",addEvent);
 
 module.exports = partnerRoutes ;
